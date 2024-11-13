c8c47ec4694efbf3d2d68d2cee471a71cde382d6
Ravindra <128573488+Ravsst2004@users.noreply.github.com>
2024-11-13 18:20:42 +0800

    Delete prisma/migrations directory

--- prisma/migrations/20241101133459_fix_thread_model/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `image` on the `Thread` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Thread" DROP COLUMN "image",
-ADD COLUMN     "images" TEXT[];
